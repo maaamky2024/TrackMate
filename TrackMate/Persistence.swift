f759b6344bab6a97cee9e49123bc93b78758ab66
Glen Mars <alex.and.chandler.belacic@gmail.com>
2026-09-25 11:36:33 -0400

    Fixed database mutations caused by viewContext during startup; now executes inside container.performBackgroundTask. Startup database writes will also no longer happen in the UI context.

--- TrackMate/Persistence.swift
+++ TrackMate/Persistence.swift
@@ -32,31 +32,44 @@ struct PersistenceController {
         return result
     }()
     
-    init(inMemory: Bool = false) {
-        let container = NSPersistentCloudKitContainer(name: "TrackMate")
+	init(inMemory: Bool = false) {
+		let container = NSPersistentCloudKitContainer(name: "TrackMate")
+		
+		if inMemory {
+			container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+		}
+		
+		let description = container.persistentStoreDescriptions.first
+		description?.shouldMigrateStoreAutomatically = true
+		description?.shouldInferMappingModelAutomatically = true
+		
+		container.loadPersistentStores { _, error in
+			if let error = error as NSError? {
+				fatalError("Unresolved error\(error), \(error.userInfo)")
+			}
+			
+			container.performBackgroundTask { backgroundContext in
+				backgroundContext.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+				backgroundContext.undoManager = nil
+				
+				ThemeSeeder.seedThemes(context: backgroundContext)
+				preloadRedFlagsIfNeeded(context: backgroundContext)
+			}
+		}
+		
+		container.viewContext.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+		container.viewContext.automaticallyMergesChangesFromParent = true
+		
+		self.container = container
+	}
+	
+	func newBackgroundContext() -> NSManagedObjectContext {
+		let context = container.newBackgroundContext()
         
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
-	    
-	    let description = container.persistentStoreDescriptions.first
-	    description?.shouldMigrateStoreAutomatically = true
-	    description?.shouldInferMappingModelAutomatically = true
-        
-        let viewContext = container.viewContext
-        
-        container.loadPersistentStores { _, error in
-            if let error = error as NSError? {
-                fatalError("Unresolved error\(error), \(error.userInfo)")
-            }
-            
-            ThemeSeeder.seedThemes(context: viewContext)
-            preloadRedFlagsIfNeeded(context: viewContext)
-        }
-        
-        viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
-        viewContext.automaticallyMergesChangesFromParent = true
-        self.container = container
+        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        context.undoManager = nil
+		
+		return context
     }
 }
 
